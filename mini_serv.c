65705f127d0b7bdbd3010ce38725f1ca9f529b9e
Ugo Theveny <utheveny42@gmail.com>
2022-10-03 18:59:06 +0200

    Final version without comment

--- mini_serv.c
+++ mini_serv.c
@@ -8,14 +8,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//Struct
 typedef struct	s_client {
 	int					fd;
 	int					id;
 	struct s_client*	next;
 }				t_client;
 
-//Close sockets and free for all clients
 void	freeClients(t_client *clients) {
 	t_client*	tmp;
 
@@ -27,7 +25,6 @@ void	freeClients(t_client *clients) {
 	}
 }
 
-//Fatal
 void	fatalError(int serverSocket, t_client *clients){
 	write(2, "Fatal error\n", 12);
 	if (serverSocket != -1)
@@ -37,7 +34,6 @@ void	fatalError(int serverSocket, t_client *clients){
 	exit(1);
 }
 
-//Add a new client to the linked list
 int		AddClient(t_client **clients, int fd, int serverSocket) {
 	static int lastId = -1;
 	t_client*	new;
@@ -60,7 +56,6 @@ int		AddClient(t_client **clients, int fd, int serverSocket) {
 	return (new->id);
 }
 
-//Remove a client from the linked list
 int		deleteClient(t_client **clients, int fd, int serverSocket) {
 	t_client*	prev;
 	t_client*	tmp;
@@ -92,7 +87,6 @@ int		deleteClient(t_client **clients, int fd, int serverSocket) {
 	return (id);
 }
 
-//Send message
 void	sendToClients(int serverSocket, t_client *clients, char* toSend, int fd) {
 	t_client *tmpClients = clients;
 
@@ -104,20 +98,16 @@ void	sendToClients(int serverSocket, t_client *clients, char* toSend, int fd) {
 	}
 }
 
-//Init socket
 int initSocket(t_client *clients, struct sockaddr_in *servaddr, char *arg){
-	// socket create and verification 
 	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (serverSocket == -1)
 		fatalError(serverSocket, clients);
-	// assign IP, PORT
 	bzero(servaddr, sizeof(*servaddr)); 
 	if (atoi(arg) <= 0)
 		fatalError(serverSocket, clients);
 	servaddr->sin_family = AF_INET; 
-	servaddr->sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr->sin_addr.s_addr = htonl(2130706433);
 	servaddr->sin_port = htons(atoi(arg));
-	// Binding newly created socket to given IP and verification 
 	if ((bind(serverSocket, (const struct sockaddr *)servaddr, sizeof(*servaddr))) != 0)
 		fatalError(serverSocket, clients);
 	if (listen(serverSocket, 10) != 0)
@@ -126,7 +116,6 @@ int initSocket(t_client *clients, struct sockaddr_in *servaddr, char *arg){
 	return (serverSocket);
 }
 
-//Init fds
 void	initFds(fd_set *set_read, int *max_fd, t_client *clients, int serverSocket){
 	t_client *tmpClients;
 	
@@ -142,90 +131,58 @@ void	initFds(fd_set *set_read, int *max_fd, t_client *clients, int serverSocket)
 	FD_SET(serverSocket, set_read);
 }
 
-//Main
 int main(int ac, char** av) {
-	//Socket
 	int					serverSocket = -1;
 	socklen_t			socketLen;
 	struct sockaddr_in	servaddr, cli;
-
-	//Client
 	t_client*			clients = NULL;
 	t_client*			tmpClients = NULL;
 	int 				clientFd;
 	int					clientId;
-
-	//FD
 	int					max_fd;
 	fd_set				set_read;
-
-	//Buffers
 	char				recvBuffer[4096 * 42];
 	char				sendBuffer[4096 * 42];
 	ssize_t				recvSize;
 
-	//Check args
 	if (ac != 2){
 		write(2, "Wrong number of arguments\n", 26);
 		exit(1);
 	}
-
-	//Init clients
 	if ((clients = malloc(sizeof(t_client))) == NULL)
 		fatalError(serverSocket, clients);
 	clients = NULL;
-	
-	//Init socket
 	serverSocket = initSocket(clients, &servaddr, av[1]);
-
-	//Loop
 	socketLen = sizeof(cli);
 	while (1) {
-		//Init FD
 		initFds(&set_read, &max_fd, clients, serverSocket);
-
-		//I check if the max_fd + 1 is set
 		if (select(max_fd + 1, &set_read, NULL, NULL, NULL) > 0) {
-			//If the FD is set
 			if (FD_ISSET(serverSocket, &set_read)) {
-				//I try to accept a new connexion
 				clientFd = accept(serverSocket, (struct sockaddr *)&cli, &socketLen);
-				//If the connexion succeed
 				if (clientFd >= 0) {
-					//Add a client
 					clientId = AddClient(&clients, clientFd, serverSocket);
 					if (max_fd < clientFd)
 						max_fd = clientFd;
-					//Fill the sendBuffer with the formated connexion message
 					sprintf(sendBuffer, "server: client %d just arrived\n", clientId);
 					sendToClients(serverSocket, clients, sendBuffer, clientFd);
 				}
 			}
-			//If the fd is not set
 			else {
-				//I use a temp copy of my clients
 				tmpClients = clients;
-				//I loop throught my clients
 				while (tmpClients != NULL) {
 					clientFd = tmpClients->fd;
 					clientId = tmpClients->id;
 					tmpClients = tmpClients->next;
-					//If the FD is set
 					if (FD_ISSET(clientFd, &set_read)) {
-						//I try to receive octets send by the fd
 						recvSize = recv(clientFd, recvBuffer, 4096 * 42, 0);
-						//A client disconnect
 						if (recvSize == 0) {
 							clientId = deleteClient(&clients, clientFd, serverSocket);
 							if (clientId != -1){
-								//Fill the sendBuffer with the formated disconnexion message
 								sprintf(sendBuffer, "server: client %d just left\n", clientId);
 								sendToClients(serverSocket, clients, sendBuffer, clientFd);
 							}
 						}
-						//Send received octets to clients
 						else if (recvSize > 0) {
-							//Fill the sendBuffer with the formated message
 							sprintf(sendBuffer, "client %d: %s", clientId, recvBuffer);
 							sendToClients(serverSocket, clients, sendBuffer, clientFd);
 							bzero(&recvBuffer, 4096 * 42);
