f4b581c649bd43fe5c8d973d3dbb4e353731ee1b
Ugo Theveny <utheveny42@gmail.com>
2022-10-08 23:09:11 +0200

    Now handling test 7

--- README.md
+++ README.md
@@ -12,42 +12,28 @@ typedef struct  s_client {
 void  freeClients(t_client *clients);
 void  fatalError(t_client *clients, int serverSocket);
 
+int   initSocket(t_client *clients, struct sockaddr_in *servaddr, char *arg);
+void  initFds(t_client *clients, int serverSocket, fd_set *setRead, int *maxFd);
+
+void  sendToClients(t_client *clients, int serverSocket, int fd, char *toSend);
+void  checkMessage(t_client *clients, int serverSocket, t_client *sender, fd_set *setRead, char *toSend);
+
 int   addClient(t_client *clients, int serverSocket, int fd);
 int   deleteClient(t_client *clients, int serverSocket, int fd);
 
-void  sendToClients(t_client *clients, int serverSocket, int fd, char *toSend);
-int   initSocket(t_client *clients, struct sockaddr_in servaddr, char *arg);
-void  initFds(t_client *clients, int serverSocket, fd_set *setRead, int *maxFd);
+int main(int ac, char** av) {
+	int					serverSocket = -1;
+	socklen_t			socketLen;
+	struct sockaddr_in	servaddr, cli;
+	t_client*			clients = NULL;
+	t_client*			tmp = NULL;
+	int 				clientFd;
+	int					clientId;
+	int					maxFd;
+	fd_set				setRead;
+	char				recvBuffer[4096 * 42];
+	ssize_t				recvSize;
 
-int   main(int ac, char **av){
-  ...
-  while (1) {
     ...
-    if (select(maxFd + 1, &setRead, NULL, NULL, NULL){
-        if (FD_ISSET(serverSocket, &setRead)){
-            ...
-            if (clientFd >= 0({
-                ...
-            }
-        }
-        else{
-            ...
-            while (tmp != NULL){
-                ...
-                if (FD_ISSET(serverSocket &setRead)){
-                    ..
-                    if (recvSize == 0){
-                        ...
-                        if (clientId != -1){
-                            ...
-                        }
-                    }
-                    else if (recvSize > 0){
-                        ...
-                    }
-                }
-            }
-        }
-    }
 }
 ```
--- mini_serv.c
+++ mini_serv.c
@@ -10,7 +10,8 @@
 typedef struct	s_client {
 	int					fd;
 	int					id;
-	struct s_client*	next;
+	char				cache[4096 * 42];
+	struct s_client		*next;
 }				t_client;
 
 void	freeClients(t_client *clients) {
@@ -66,31 +67,63 @@ void	initFds(t_client *clients, int serverSocket, fd_set *setRead, int *maxFd){
 	FD_SET(serverSocket, setRead);
 }
 
-int		addClient(t_client **clients, int serverSocket, int fd) {
+void	sendToClients(t_client *clients, int serverSocket, int fd, char* toSend) {
+	t_client *tmp = clients;
+
+	while (tmp != NULL) {
+		if (tmp->fd != fd)
+			if (send(tmp->fd, toSend, strlen(toSend), 0) < 0)
+				fatalError(clients, serverSocket);
+		tmp = tmp->next;
+	}
+}
+
+void sendMessage(t_client *clients, int serverSocket, t_client *sender, fd_set *setRead, char *toSend){
+    int i = 0;
+    int clientMsgSize = strlen(sender->cache) - 1;
+    char sendBuffer[4096 * 43];
+
+
+    while(toSend[i]){
+        sender->cache[strlen(sender->cache)] = toSend[i];
+        clientMsgSize++;
+        if (toSend[i] == '\n'){
+            sprintf(sendBuffer, "client %d: %s", sender->id, sender->cache);
+            sendToClients(clients, serverSocket, sender->fd, sendBuffer);
+            bzero(&sender->cache, 4096 * 42);
+        }
+        i++;
+    }
+}
+
+void		addClient(t_client **clients, int serverSocket, int fd) {
 	static int lastId = -1;
 	t_client*	new;
-	t_client*	tmp;
+	t_client*	tmp = *clients;
+    char        sendBuffer[128];
 
 	if ((new = malloc(sizeof(t_client))) == NULL)
 		fatalError(*clients, serverSocket);
 	new->fd = fd;
 	new->id = ++lastId;
+	bzero(&new->cache, 4096 * 42);
 	new->next = NULL;
 	if (*clients == NULL)
 		*clients = new;
 	else {
-		tmp = *clients;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = new;
 	}
-	return (new->id);
+    sprintf(sendBuffer, "server: client %d just arrived\n", new->id);
+	sendToClients(*clients, serverSocket, fd, sendBuffer);
 }
 
-int		deleteClient(t_client **clients, int fd) {
+void		deleteClient(t_client **clients, int serverSocket, int fd) {
 	t_client*	prev = NULL;
 	t_client*	tmp = *clients;
 	int			id = -1;
+    char        sendBuffer[128];
 
 	if (tmp != NULL && tmp->fd == fd) {
 		*clients = tmp->next;
@@ -110,17 +143,9 @@ int		deleteClient(t_client **clients, int fd) {
 			free(tmp);
 		}
 	}
-	return (id);
-}
-
-void	sendToClients(t_client *clients, int serverSocket, int fd, char* toSend) {
-	t_client *tmp = clients;
-
-	while (tmp != NULL) {
-		if (tmp->fd != fd)
-			if (send(tmp->fd, toSend, strlen(toSend), 0) < 0)
-				fatalError(clients, serverSocket);
-		tmp = tmp->next;
+    if (id != -1){
+		sprintf(sendBuffer, "server: client %d just left\n", id);
+		sendToClients(*clients, serverSocket, fd, sendBuffer);
 	}
 }
 
@@ -135,7 +160,6 @@ int main(int ac, char** av) {
 	int					maxFd;
 	fd_set				setRead;
 	char				recvBuffer[4096 * 42];
-	char				sendBuffer[4096 * 42];
 	ssize_t				recvSize;
 
 	if (ac != 2){
@@ -149,48 +173,24 @@ int main(int ac, char** av) {
 		if (select(maxFd + 1, &setRead, NULL, NULL, NULL) > 0) {
 			if (FD_ISSET(serverSocket, &setRead)) {
 				clientFd = accept(serverSocket, (struct sockaddr *)&cli, &socketLen);
-				if (clientFd >= 0) {
-					clientId = addClient(&clients, serverSocket, clientFd);
-					sprintf(sendBuffer, "server: client %d just arrived\n", clientId);
-					sendToClients(clients, serverSocket, clientFd, sendBuffer);
-				}
+				if (clientFd >= 0)
+                    addClient(&clients, serverSocket, clientFd);
 			}
 			else {
 				tmp = clients;
 				while (tmp != NULL) {
-					clientFd = tmp->fd;
-					clientId = tmp->id;
-					tmp = tmp->next;
-					if (FD_ISSET(clientFd, &setRead)) {
-						recvSize = recv(clientFd, recvBuffer, 4096 * 42, 0);
-						if (recvSize == 0) {
-							clientId = deleteClient(&clients, clientFd);
-							if (clientId != -1){
-								sprintf(sendBuffer, "server: client %d just left\n", clientId);
-								sendToClients(clients, serverSocket, clientFd, sendBuffer);
-							}
-						}
+					if (FD_ISSET(tmp->fd, &setRead)) {
+						recvSize = recv(tmp->fd, recvBuffer, 4096 * 42, 0);
+						if (recvSize == 0){
+                            deleteClient(&clients, serverSocket, tmp->fd);
+                            break;
+                        }
 						else if (recvSize > 0) {
-							int i = 0;
-							int j = 0;
-							char msg[4096 * 42];
-							bzero(&msg, 4096 * 42);
-							while (recvBuffer[i]){
-								if (recvBuffer[i] == '\n'){
-									sprintf(sendBuffer, "client %d: %s\n", clientId, msg);
-									sendToClients(clients, serverSocket, clientFd, sendBuffer);
-									bzero(&msg, 4096 * 42);
-									j = 0;
-								}
-								else {
-									msg[j] = recvBuffer[i];
-									j++;
-								}
-								i++;
-							}
-							bzero(&recvBuffer, 4096 * 42);
+							sendMessage(clients, serverSocket, tmp, &setRead, recvBuffer);
+                            bzero(&recvBuffer, 4096 * 42);
 						}
 					}
+                    tmp = tmp->next;
 				}
 			}
 		}
